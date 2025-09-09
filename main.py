f7fb9908f5371ed461cb885b0bb23ff6239bbffb
Iris Young <irisdyoung@gmail.com>
2025-09-09 07:24:28 +0200

    Move scattering factor calc/lookup to a class

--- main.py
+++ main.py
@@ -5,52 +5,88 @@ import gemmi
 # Assemble products of structure factors for each pair of atom type in a
 # standard structure (rare types can be added later via exception handling)
 
-# Standardize here how to get scattering factor -- reused below
-def get_sf(gemmi_element, scatterer="xray", anom=False, energy=None, wavelength=None, stol2=0.4):
+class ScatteringLookup:
     """Calculate scattering by one of several methods depending on type of radiation.
     Argument gemmi_element is the gemmi object, from which the element name or atomic
     number will be fetched. All other default arguments will calculate non-anomalous
     scattering for an X-ray source. For documentation on these implementations, see
     https://gemmi.readthedocs.io/en/latest/scattering.html."""
-    def get_energy():
-        assert (energy is not None) or (wavelength is not None), "either wavelength (Å) or energy (eV) needed for calculation of scattering factor"
-        assert (energy is None) or (wavelength is None), "conflict: please supply either wavelength (Å) or energy (eV), but not both"
+    def __init__(self, scatterer="xray", anom=False, energy=None, wavelength=None, stol2=0.4):
+        self.scatterer = scatterer
+        self.anom = anom
+        assert (not anom) or (energy is not None) or (wavelength is not None), \
+            "either wavelength (Å) or energy (eV) needed for calculation of anomalous scattering factors"
+        assert (energy is None) or (wavelength is None), \
+            "conflict: please supply either wavelength (Å) or energy (eV), but not both"
         if wavelength is not None:
             energy = gemmi.hc/wavelength
-        return energy
-    match scatterer:
-        case "xray":
-            if anom:
-                # use Cromer-Liberman algorithm for calculating X-ray scattering
-                # factors with f' and f" contributions
-                coeffs = gemmi.cromer_liberman(z=gemmi_element.atomic_number, energy=get_energy())
-            else:
-                # use X-ray scattering factors tabulated in the International Tables
-                # of Crystallography (1992) without anomalous scattering
-                coeffs = gemmi.Element(gemmi_element).it92
-        case "electron":
-            # use 5-Gaussian approximation of electron form factors as described in
-            # the International Tables of Crystallography C (2011): table 4.3.2.2
-            assert not anom, "anomalous scattering factors not available for electrons"
-            coeffs = gemmi_element.c4322
-        case "neutron":
-            # use bound coherent scattering lengths from NCNR (1992) for neutrons
-            assert not anom, "anomalous  scattering factors not available for neutrons"
-            coeffs = gemmi_element.neutron92
-    return coeffs.calculate_sf(stol2=stol2)
+        self.energy = energy
+        self.stol2 = stol2 # stol2 is (sin(theta)/lambda)^2
 
-# Set up lookup table for scattering factors (can be updated in place by the methods
-# that use it if an element not listed here is encountered)
-single_element_SFs = {
-    element: get_sf(element) for element in ('C','N','O','H','S')
-}
-pair_SFs = {
-    (e1, e2):single_element_SFs[e1]*single_element_SFs[e2] \
-    for e1 in single_element_SFs.keys() \
-    for e2 in single_element_SFs.keys()
-}
-# There are duplicate entries for pairs in reversed order, e.g. both
-# ('C','O') and ('O','C'), but we will treat this as a feature, not a bug!
+        # Set up lookup table for scattering factors (can be updated in place by the
+        # methods that use it if an element not listed here is encountered)
+        self.single_element_SFs = {
+            element: self.calculate_sf(element) for element in ('C','N','O','H','S')
+        }
+        self.pair_SFs = {
+            (e1, e2):self.single_element_SFs[e1]*self.single_element_SFs[e2] \
+            for e1 in self.single_element_SFs.keys() \
+            for e2 in self.single_element_SFs.keys()
+        }
+        # There are duplicate entries for pairs in reversed order, e.g. both
+        # ('C','O') and ('O','C'), but we will treat this as a feature, not a bug!
+
+    def calculate_sf(self, element, stol2=None):
+        """Calculate each atomic scattering factor (once). Element should be supplied
+        as a string, as the 1-to-2-letter code. (sin(theta)/lambda)^2 can be
+        overridden here but normally will be whatever was passed to __init__."""
+        gemmi_element = gemmi.Element(element)
+        match self.scatterer:
+            case "xray":
+                if self.anom:
+                    # use Cromer-Liberman algorithm for calculating X-ray scattering
+                    # factors with f' and f" contributions
+                    get_coeffs = gemmi.cromer_liberman(z=gemmi_element.atomic_number, energy=self.energy)
+                else:
+                    # use X-ray scattering factors tabulated in the International Tables
+                    # of Crystallography (1992) without anomalous scattering
+                    get_coeffs = gemmi_element.it92
+            case "electron":
+                # use 5-Gaussian approximation of electron form factors as described in
+                # the International Tables of Crystallography C (2011): table 4.3.2.2
+                assert not self.anom, "anomalous scattering factors not available for electrons"
+                get_coeffs = gemmi_element.c4322
+            case "neutron":
+                # use bound coherent scattering lengths from NCNR (1992) for neutrons
+                assert not self.anom, "anomalous  scattering factors not available for neutrons"
+                get_coeffs = gemmi_element.neutron92
+        return get_coeffs.calculate_sf(stol2=(stol2 or self.stol2))
+
+    def get_sf(self, element_pair):
+        """Look up a product of two elemental scattering factors. Calcualte only on
+        the first lookup. Pass a tuple element_pair of element code strings to this
+        method, e.g. ('O','S')."""
+        try:
+            return self.pair_SFs[element_pair]
+        except KeyError:
+            # throw and catch an exception when the pair's scattering factor
+            # product is not in the lookup table already, and try to add it.
+            sfs = []
+            for elem in element_pair:
+                try:
+                    # first attempt lookup (should still work for one atom in most
+                    # pairs where the other atom's scattering must be calculated)
+                    sfs.append(self.single_element_SFs[elem])
+                except KeyError:
+                    try:
+                        # calculate the first time encountering each uncommon element
+                        sfs.append(self.calculate_sf(elem))
+                    except AttributeError:
+                        # may still fail on especially heavy elements such as Nh
+                        assert False, f"Failed to calculate scattering for element {elem}"
+            # compute product of scattering factors and update lookup
+            self.pair_SFs[element_pair] = scattering = sfs[0] * sfs[1]
+            return scattering
 
 class PattersonSim:
     """Determine the expected positions and intensities of peaks in the
@@ -60,6 +96,9 @@ class PattersonSim:
         # load atomistic model from PDB or mmCIF file
         self.model = gemmi.read_structure(model_path)
         self.get_atom_pairs()
+        self.scattering_lookup = ScatteringLookup()
+        # TODO: initialize lookup with args drawn from the model file header
+        # and anomalous if possible
         self.get_peaks()
 
     def get_atom_pairs(self, include_h=True, include_water=True):
@@ -93,21 +132,7 @@ class PattersonSim:
             if not include_h and (atom1.element.name == 'H' or atom2.element.name == 'H'): continue
 
             # get the product of the scattering factors from the two atoms
-            try:
-                scattering = pair_SFs[(atom1.element.name, atom2.element.name)]
-            except KeyError:
-                # throw and catch an exception when the pair's scattering factor
-                # product is not in the lookup table already, and add it if it can
-                # be calculated from the ITC. Warn, but continue, if it can't be
-                # calculated from the table values.
-                try:
-                    scattering = get_sf(atom1.element.name) * get_sf(atom2.element.name)
-                except AttributeError:
-                    # possible for metals and other heavier elements, e.g. Nh
-                    print(f"Warning: ignoring atom pair {atom1.element.name}, {atom2.element.name}")
-                    continue
-                # update lookup and continue
-                pair_SFs[(atom1.element.name, atom2.element.name)] = scattering
+            scattering = self.scattering_lookup.get_sf((atom1.element.name, atom2.element.name))
 
             # scale to account for occupancy
             scattering *= (atom1.occ * atom2.occ)
